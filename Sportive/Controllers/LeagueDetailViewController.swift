7a8a130ef46059f2369d336ad46ad352f6bb2f96
Ahmed-Abdelraouf <eng.ahmed.elsaadny@gmail.com>
2021-03-22 16:40:15 +0200

    Details Table View First Scene

--- Sportive/Controllers/LeagueDetailViewController.swift
+++ Sportive/Controllers/LeagueDetailViewController.swift
@@ -8,24 +8,39 @@
 import UIKit
 
 class LeagueDetailViewController: UIViewController {
+    
     @IBOutlet weak var eventsToolBar: UIToolbar!
+    @IBOutlet weak var detailTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        detailTableView.delegate = self
+        detailTableView.dataSource = self
+
+    }
+    
+    
+}
+
+extension LeagueDetailViewController: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-    }
+            if let cell = self.detailTableView.dequeueReusableCell(withIdentifier: "LeugueDetailsTableViewCell") as? LeugueDetailsTableViewCell {
+                
+                cell.firstTeamImageView?.image = UIImage(named: "premierLeague.jpg")
+                cell.secondTeamImageView?.image = UIImage(named: "premierLeague.jpg")
+                return cell
+            }
+            
+            return UITableViewCell()
+        
     
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+}
 }
